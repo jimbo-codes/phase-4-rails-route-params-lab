9d70df56ad657e605d54f1df4b680612dc8428c1
James Burns <jrburns96@aol.com>
2022-01-11 16:20:02 -0500

    commit tree 80ddff454c3a3a2e307c128372b99864c76a1920

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
+    student = Student.find_by_last_name(params[:name])
+    byebug
     students = Student.all
     render json: students
   end
 
+  def show
+    student = Student.find_by_id(params[:id])
+    render json: student
+  end
+
+  def test
+    byebug
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # get 'students?key1=value1&key2=value2', to: 'students#test'
+
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  get 'students/:id', to: 'students#show'
+
 end
